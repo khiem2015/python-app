e2e3649a19c3f50861e6a84ec87cdc419bf127ef
khiem2015 <khiemnguyen2501@gmail.com>
2025-08-16 19:58:30 +0700

    push

--- program.py
+++ program.py
@@ -158,11 +158,13 @@ class Home(QWidget):
 
         self.id = id
         self.user = get_user_by_id(id)
+        self.load_user_info()
         
         self.stack_widget = self.findChild(QStackedWidget, "stackedWidget")
         self.btn_home = self.findChild(QPushButton, "btn_home")
         self.btn_profile = self.findChild(QPushButton, "btn_profile")
         self.btn_detail = self.findChild(QPushButton, "btn_detail")
+        self.btn_save_account = self.findChild(QPushButton, "btn_save_account")
 
         self.txt_name = self.findChild(QLineEdit, "txt_name")
         self.txt_email = self.findChild(QLineEdit, "txt_email")
@@ -172,6 +174,7 @@ class Home(QWidget):
 
         self.btn_home.clicked.connect(lambda: self.navigate_screen(self.stack_widget, 0))
         self.btn_profile.clicked.connect(lambda: self.navigate_screen(self.stack_widget, 1))
+        self.btn_save_account.clicked.connect(self.update_user_info)
 
     def navigate_screen(self, stackWidget: QStackedWidget, index: int):
         stackWidget.setCurrentIndex(index)
@@ -183,15 +186,20 @@ class Home(QWidget):
         self.txt_gender.setCurrentText(self.user["gender"])
         self.btn_avatar.setIcon(QIcon(self.user["avatar"]))
 
-
-
-
     def update_avatar(self):
         file,_ = QFileDialog.getOpenFileName(self,"Select Image","","Image Files(*.png *.jpg *jpeg *.bmp)")
         if file:
             self.user["avatar"] = file
             self.btn_avatar.setIcon(QIcon(file))
             update_user_avatar(self.id, file)
+
+    def update_user_info(self):
+        name = self.txt_name.text().strip()
+        birthday = self.txt_birthday.date().toString("dd/MM/yyyy")
+        gender = self.txt_gender.currentText()
+        update_user(self.id, name, birthday, gender)
+        msg.success_message("Update", "User info updated seccessfully")
+        self.load_user_info()
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
